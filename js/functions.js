b1b6953670db9715ba6133e0d566b0acfad416a5
ErnestoGarSa <ernest0_10@hotmail.com>
2022-05-24 13:59:31 -0500

    ADD Homework

--- js/functions.js
+++ js/functions.js
@@ -16,88 +16,87 @@
 
 //Invocar la funcion tenemos que abrir y cerrar los parentesis
 
-// function miPrimerFuncion (){
-//     console.log("Mi primer función")
-// }
+function miPrimerFuncion (){
+    console.log("Mi primer función")
+}
 
-// miPrimerFuncion()
+miPrimerFuncion()
 
-// function otraFuncion () {
-//     console.log("Esta es otra función")
-// }
+function otraFuncion () {
+    console.log("Esta es otra función")
+}
 
-// otraFuncion()
+otraFuncion()
 
-// function reverseString(){
-//     let strUser = prompt("Dame una palabra", "str").toLowerCase()
-//     let strReverse=""
+function reverseString(){
+    let strUser = prompt("Dame una palabra", "str").toLowerCase()
+    let strReverse=""
 
-//     for (let i = strUser.length - 1 ; i >= 0 ; i--){
-//         strReverse+= strUser[i]
-//         // console.log(strReverse)
-//         } console.log(strReverse)
-// }
+    for (let i = strUser.length - 1 ; i >= 0 ; i--){
+        strReverse+= strUser[i]
+        // console.log(strReverse)
+        } console.log(strReverse)
+}
 
-// function getNumber(){
-//     let temporalNumber= Number(prompt("Dame un número"))
-//     console.log(temporalNumber)
-// } 
+function getNumber(){
+    let temporalNumber= Number(prompt("Dame un número"))
+    console.log(temporalNumber)
+} 
 
-// function addDivisors(){
-//     let suma=0
-//     for (let i = 1 ; i <= 100 ; i++){
-//         if (i % 3 == 0 ){
-//             suma+=i
-//         } else if (i % 5 == 0){
-//             suma+=i
-//         } else if( i % 7 == 0){
-//             suma+=i
-//         }
-//     } console.log(suma)
-// }
-
-
-// function addNumbers(){
-//     let counter= Number(prompt("Dame un número:"))
-//     let suma=0
-//     while (counter >= 0){
-//         suma+=counter
-//         counter--
-//     }
-//     console.log(suma)
-// }
+function addDivisors(){
+    let suma=0
+    for (let i = 1 ; i <= 100 ; i++){
+        if (i % 3 == 0 ){
+            suma+=i
+        } else if (i % 5 == 0){
+            suma+=i
+        } else if( i % 7 == 0){
+            suma+=i
+        }
+    } console.log(suma)
+}
 
 
-// function addCharacter(){
-//     let string = prompt("Dame una oración:")
-//     let fin = string.length - 1 
-//     let counter2 = 0
+function addNumbers(){
+    let counter= Number(prompt("Dame un número:"))
+    let suma=0
+    while (counter >= 0){
+        suma+=counter
+        counter--
+    }
+    console.log(suma)
+}
 
-//     let vowelsA=0
-//     let vowelsE=0
-//     let vowelsS=0
 
-//     do{
-//         switch (string.toLowerCase()[counter2]){
-//             case "a":
-//                 vowelsA++
-//                 break
-//             case "e":
-//                 vowelsE++
-//                 break
-//             case " ":
-//                 vowelsS++
-//                 break
-//             default:
-//                 break
-//         }
-//         counter2++
-//     }while (counter2 <= fin)
-//     console.log(`Lestas "a" son: ${vowelsA}`)
-//     console.log(`Lestas "e" son: ${vowelsE}`)
-//     console.log(`Espacios son: ${vowelsS}`)
-// }
+function addCharacter(){
+    let string = prompt("Dame una oración:")
+    let fin = string.length - 1 
+    let counter2 = 0
 
+    let vowelsA=0
+    let vowelsE=0
+    let vowelsS=0
+
+    do{
+        switch (string.toLowerCase()[counter2]){
+            case "a":
+                vowelsA++
+                break
+            case "e":
+                vowelsE++
+                break
+            case " ":
+                vowelsS++
+                break
+            default:
+                break
+        }
+        counter2++
+    }while (counter2 <= fin)
+    console.log(`Lestas "a" son: ${vowelsA}`)
+    console.log(`Lestas "e" son: ${vowelsE}`)
+    console.log(`Espacios son: ${vowelsS}`)
+}
 
 
 
@@ -107,49 +106,50 @@
 //Return termina la ejecucion de la función
 //Return devuelve un valor
 
-// function getone(){
-//     let num=10
-//     return num
+function getone(){
+    let num=10
+    return num
 
-//     console.log("pasos intermedios")
-// }
+    console.log("pasos intermedios")
+}
 
-// getone()
+getone()
 
-// function suma(){
-//     return 8 + 10
-// }
-// //Invocando la función
-// let diezYOcho= suma()
+function suma(){
+    return 8 + 10
+}
 
-// console.log(diezYOcho )
+//Invocando la función
+let diezYOcho= suma()
 
-// let hola= function(){
-//     return "Hola"
-// }
+console.log(diezYOcho )
 
-// console.log(hola)
+let hola= function(){
+    return "Hola"
+}
+
+console.log(hola)
 
 
 //Funciones con Parametros 
 //Los parametros van adentro de los parentesis y puede ser cualquier numero de parametros
 
-// function suma1(a, b){
-//     let total=0
-//     total= a + b
-//     return total
-// }
+function suma1(a, b){
+    let total=0
+    total= a + b
+    return total
+}
 
-// console.log(suma1(10,34))
+console.log(suma1(10,34))
 
-// //Funciones con parametros opcionales
+//Funciones con parametros opcionales
 
-// function suma2(a, b, c = 2){
-//     return a + b + c
-// }
+function suma2(a, b, c = 2){
+    return a + b + c
+}
 
-// console.log(suma2(5,6))
-// console.log(suma2(5,6,8))
+console.log(suma2(5,6))
+console.log(suma2(5,6,8))
 
 
 //Ejercicio 
---
+++ js/homework.js
@@ -0,0 +1,111 @@
+/**
+ * Funcion que pida una palabra al usuario
+ * Invierta la palabra
+ * y la retorne en un alert
+ * reversedString('hola') -> 'aloh' 
+ */
+
+function reversedString(word){
+    let stringReverse=""
+    let fin = word.length - 1
+
+    for (let i = fin; i >= 0 ; i--){
+        stringReverse+= word[i]   
+    }window.alert(stringReverse)
+}
+
+let string= prompt("Por favor ingresa una palabra: ")
+let callReverse = reversedString(string)
+console.log(callReverse)
+
+
+/**
+ * Funcion que pida 3 numeros como parametro
+ * 2 numeros obligatorios y 1 opcional con valor de 3
+ * Arroje la suma de esos 3 numeros
+ * Posibles resultados
+ * -> addThreeNumbers(3,4,5) -> 12
+ * -> addThreeNumbers(3,4) -> 10
+ * -> addThreeNumbers(3) -> 'Faltan datos'
+ * -> addThreeNumbers() -> 'Faltan datos'
+ */
+
+function userAdd (a, b, c=3){
+    if (a === null || a ==="" || a === undefined || b === null || b ==="" || b === undefined){
+        window.alert("Faltan datos")
+    } else {
+        window.alert(`La suma de los 3 numeros es ${Number(a) + Number(b) + Number(c)}`)
+    }
+}
+
+let num1= prompt("Por favor ingresa el primer valor: ")
+let num2= prompt("Por favor ingresa el segundo valor: ")
+let num3= prompt("Por favor ingresa el tercer valor: ")
+
+let callAdd= userAdd(num1, num2, num3)
+console.log(callAdd)
+
+
+
+/**
+ * Funcion que pida al usuario los grados Centigrados en su localidad
+ * Convertirlos a grados fahrenheit
+ * Arrojar un alert con el resultado
+ * -> convertTemp(25) -> La temperatura es de 77 ºF
+ * -> convertTemp() -> 'Faltan datos'
+ */
+
+function CelsiusFahrenheit(cel){
+    if (cel === null || cel ==="" || cel === undefined){
+        window.alert("Faltan datos")
+    } else {
+        fahrenheit= Number(cel) * 9/5 + 32
+        let mensaje=`Los grados Fahrenheit equivalentes a ${cel} Ceslsius son: ${fahrenheit}`
+        window.alert(mensaje)
+    }
+}
+
+let grados= prompt("Por favor ingresa los grados Celsius de tu localidad: ")
+let convertir=CelsiusFahrenheit(grados)
+console.log(convertir)
+ 
+
+
+
+/**
+ * Funcion que imprima las tablas del 1 al 10
+ */
+
+function tablas(){
+    for (let counter=1 ; counter <= 10 ; counter++){
+        for (let i=1; i<=10; i++){
+            console.log(`${counter} x ${i} = ${counter * i}`)
+        }
+    console.log("*****************")
+    }
+}
+
+
+/**
+ * Funcion que pida al usuario un numero (N) entre 1 y 100 
+ * Mandar un alert con suma de 1 a N
+ * Valor por defecto 3
+ * addNumberLimit() -> 6
+ * addNumberLimit(4) -> 10
+ * addNumberLimit(5) -> 15
+ */
+
+function addNumber(N=3){
+    if( N >= 1 && N <= 100){
+        let total= 0
+        for (let i= 1 ; i <= N; i++){
+            total += i
+        } window.alert(total)
+    } else{
+        window.alert("Ingresaste un valor invalido")
+    }
+}
+
+let numb=Number(prompt("Por favor ingresa un número: "))
+let cadenaSumas=addNumber(numb)
+console.log(cadenaSumas)
