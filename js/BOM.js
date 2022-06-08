5fcc8ce374950317026c4a1aa98f37fce5d632b2
ErnestoGarSa <ernest0_10@hotmail.com>
2022-06-08 14:31:08 -0500

    ADD class files

---
+++ js/BOM.js
@@ -0,0 +1,30 @@
+// DOM
+// Manipula el documento HTML
+// document.querySelector("item")
+// element.classList.add("nuevaClase")
+// element.innerHTML = "nuevoHTML"
+// element.addAttribute("propiedad", "valor"")
+// element.getAttribute("propiedad")
+// document.getElementsByTagName("h1")[1]
+// document.createElement("h3")
+// document.getElementById("idDeInput").value
+// element.addEventListener("click", callback)
+// element.addEventListener("clicl", () => {......})
+
+// Browser Object Model (BOM)
+// Comunicarse con el navegador
+
+// window
+// window.innerWidth;
+// window.innerHeight;
+
+// window.screen
+// *
+
+// window.navigator
+// *
+
+// window.history.back()
+// window.history.forward()
+
+// window.location
