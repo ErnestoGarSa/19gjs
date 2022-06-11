4d75648bdce56cf7e0539d4d4b2e90840b2cfced
ErnestoGarSa <ernest0_10@hotmail.com>
2022-06-10 20:09:05 -0500

    ADD homework changes

--- js/homework.js
+++ js/homework.js
@@ -1,72 +1,75 @@
 let enviar = document.querySelector(".enviar").addEventListener("click", () => {
 
-post={
-    title : document.querySelector("#title").value,
-    body : document.querySelector("#body").value,
-    date : document.querySelector("#date").value
-}
+    let title =document.querySelector("#title").value.trim()
+    let body =document.querySelector("#body").value.trim()
+    let date =document.querySelector("#date").value.trim()
 
-const request = new XMLHttpRequest()
+    if( title !== "" && body !== "" && date !== "" ){
 
-request.open("POST", "https://js19g-8ff6a-default-rtdb.firebaseio.com/posts/.json", true )
+        post={
+            title : title, 
+            body : body,
+            date : date,
+        }
 
-request.onload = function () {
-    console.log("onload complete")
-}
+        const request = new XMLHttpRequest()
 
-request.send(JSON.stringify(post))
-alert("Muchas gracias, en breve checaremos tu comentario")
+        request.open("POST", "https://js19g-8ff6a-default-rtdb.firebaseio.com/posts/.json", true )
 
+        request.onload = function () {
+            console.log("onload complete")
+        }
+
+        request.send(JSON.stringify(post))
+        // alert("Muchas gracias, en breve checaremos tu comentario")
+    }
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Falta terminar 
-
-const commentsFilter = (arrIPost) => {
-    let template = arrIPost.reduce((acc, comment) => {
+const obtPosts = (arrPosts) => {
+    let template = arrPosts.reduce((acc, post) => {
         return acc +=`
         <div class="col mb-4">
               <div class="card border-primary">
                 <div class="card-body bg-dark">
-                  <h5 class="card-title bg-success text-white p-2">{{ comment.${comment.title}</h5>
-                  <p class="card-text bg-primary text-white p2">{{ comment.${comment.body}</p>
-                  <p class="card-text bg-warning p2">${comment.date}</p>
+                  <h5 class="card-title bg-success text-white p-2">${post.title}</h5>
+                  <p class="card-text bg-primary text-white p2">${post.body}</p>
+                  <p class="card-text bg-warning p2">${post.date}</p>
                 </div>
               </div>
             </div>
         `
     },"")
-    document.querySelector(".comments").innerHTML = template
+    document.querySelector(".container").innerHTML = template
 }
 
-const call = new XMLHttpRequest()
+const getPost = new XMLHttpRequest()
 
-let comments=[]
+let posts= []
 
-call.onload = (response) =>{
+getPost.onload = (response) =>{
     if (response.target.status === 200 && response.target.responseText != ""){
         let JSONResponse = JSON.parse(response.target.responseText)
         console.log(JSONResponse)
-        comments= JSONResponse
-        commentsFilter(comments)
+        posts= JSONResponse
+        obtPosts(comments)
     }
 }
 
-call.open("GET", "https://js19g-8ff6a-default-rtdb.firebaseio.com/posts", true)
+getPost.open("GET", "https://js19g-8ff6a-default-rtdb.firebaseio.com/posts/.json", true)
 
-call.send()
+getPost.send()
+
+document.querySelector(".ver").addEventListener("click", obtPosts)
+
+
+
+
+
+
+
+
+
+
+
+
